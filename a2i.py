3634ca71866f9808d21461c4005dfd1daf9733d7
Trung Nguyễn Phú <134665760+boo283@users.noreply.github.com>
2023-11-18 08:39:53 +0700

    Update a2i: edit file name of PNG's output matching the original apk file

--- a2i.py
+++ a2i.py
@@ -5,6 +5,7 @@ from math import sqrt, ceil
 
 def convert_apk_to_img(input_file, output_location, image_format):
     output_filename = ''
+    name = pathlib.Path(input_file).stem
     data = bytearray()
     with zipfile.ZipFile(input_file, 'r') as apkFile:
         for file in sorted(apkFile.namelist()):
@@ -13,18 +14,22 @@ def convert_apk_to_img(input_file, output_location, image_format):
 
     if output_location[len(output_location) - 1] != '/':
         output_location += '/'
-    output_filename = output_location + hashlib.md5(data).hexdigest() + '.png'
+    #output_filename = output_location + hashlib.md5(data).hexdigest() + '.png'
     
+    type = '_GrayScale'
     data_len = len(data)
     d = np.frombuffer(data, dtype=np.uint8)
     img_size = int(ceil(sqrt(data_len))) 
     pad_len = img_size * img_size - data_len
     shape = (img_size, img_size)
     if image_format == "RGB":
+        type = '_RGB'
         img_size = ceil(sqrt(ceil(data_len / 3)))
         pad_len = img_size * img_size * 3 - data_len
         shape = (img_size, img_size, 3)
 
+    name += type
+    output_filename = output_location + name + '.png'
     padded_d = np.hstack((d, np.zeros(pad_len, np.uint8)))
     im = np.reshape(padded_d, shape)
     cv2.imwrite(output_filename, im)
